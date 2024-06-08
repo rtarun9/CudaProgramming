295674c0e0d90ad079c8cfed4ac5c9a93bdbe134
rtarun9 <rtarun0913@gmail.com>
2024-06-08 16:13:41 +0530

    Updated unified memory sample to use prefetch mem async, added support for multiple thread blocks in grid in vector addition sample

--- src/unified_memory.cu
+++ src/unified_memory.cu
@@ -2,37 +2,65 @@
 // from both the cpu and gpu.
 // In GPU Api programming terms, it is the region of memory that is not as fast as 
 // only host / only device, but can be accessed by both.
+// HOW IT WORKS:
+// In modern GPU architectures, memory may not be allocated when cudaMallocManaged is called().
+// The memory & page table entries are not created until the data is accessed by GPU or CPU.
+// If data is on CPU and then required by GPU, page table entries must be migrated, which has slight overhead.
+// Pre-fetching can hint the driver to explicitely migrate memory before use.
 
 #include <stdio.h>
 #include <stdlib.h>
 
-__global__ void multiply_by_factor(int* array, int factor)
+__global__ void multiply_by_factor(int* input, int* output, int factor)
 {
     int index = threadIdx.x + blockIdx.x * blockDim.x;
 
-    array[index] = array[index] * factor;
+    output[index] = input[index] * factor;
+}
+
+__global__ void fill_data(int* input)
+{
+    int index = threadIdx.x + blockIdx.x * blockDim.x;
+    input[index] = index;
 }
 
 int main()
 {
     const int N = 32;
-    int *array = NULL;
+    int* input_array{nullptr};
+    int* output_array{nullptr};
 
-    cudaMallocManaged((void**)&array, sizeof(int) * N);
+    cudaMallocManaged(&input_array, sizeof(int) * N);
+    cudaMallocManaged(&output_array, sizeof(int) * N);
 
+    fill_data<<<1, N>>>(input_array);
+
+    // Nwo, if twe access this memory from the CPU, then when kernel is launched we have to move data to GPU (since page fault will happen).
+    // To prevent this, fill in data on the GPU directly. For arrays where no data is to be filled, you can prefetch and hint the driver that 
+    // the memory will PROBABLY be required for use by GPU.
+    #if 0
     for (int i = 0; i < N; i++)
     {
         array[i] = i;
     }
+    #endif
 
-    multiply_by_factor<<<1, N>>>(array, 2);
+    int device_id = 0;
+    cudaGetDevice(&device_id);
+
+    cudaMemPrefetchAsync(&output_array, sizeof(int) * N, device_id);
+
+    multiply_by_factor<<<1, N>>>(input_array, output_array, 2);
 
     // Before accesing the array (ptr) on host side, wait for the gpu to finish.
+    // In regular (non - unified) memory access, stuff like memcpy is essentially a memory barrier, which is why this device synchronize was not required.
     cudaDeviceSynchronize();
 
+
+    // When the data is being read / written to by CPU, the driver will automagically handle the barriers and other cases for us.
     for (int i = 0; i < N; i++)
     {
-        printf("array[%d] = %d.\n", i, array[i]);
+        printf("array[%d] = %d.\n", i, output_array[i]);
     }
 
     return 0;
--- src/vector_addition.cu
+++ src/vector_addition.cu
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define VECTOR_DIMENSION 32
+#define VECTOR_DIMENSION 32 * 32 * 11 * 9 * 33
 
 __global__ 
 void vector_add(const int* const vector_a, const int* const vector_b, int *const vector_sum_result)
 {
-    int i = threadIdx.x;
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
 
-    // Copy data from global memory to registers.
-    int a = vector_a[i];
-    int b = vector_b[i];
+    if (i < VECTOR_DIMENSION)
+    {
+        // Copy data from global memory to registers.
+        int a = vector_a[i];
+        int b = vector_b[i];
 
-    vector_sum_result[i] = a + b;
+        vector_sum_result[i] = a + b;
+    }
 }
 
 int main()
@@ -46,8 +49,8 @@ int main()
     cudaMemcpy(device_vector_b, host_vector_b, sizeof(int) * VECTOR_DIMENSION, cudaMemcpyKind::cudaMemcpyHostToDevice);
 
     // Invoke the kernel to perform processing on the GPU.
-    const dim3 block_dim(1, 1, 1);
-    const dim3 thread_group_dim(VECTOR_DIMENSION, 1, 1);
+    const dim3 thread_group_dim(64, 1, 1);
+    const dim3 block_dim((VECTOR_DIMENSION + thread_group_dim.x - 1) / thread_group_dim.x, 1, 1);
 
     vector_add<<<block_dim, thread_group_dim>>>(device_vector_a, device_vector_b, device_vector_sum_result);
 
@@ -55,6 +58,7 @@ int main()
     cudaMemcpy(host_vector_sum_result, device_vector_sum_result, VECTOR_DIMENSION * sizeof(int), cudaMemcpyKind::cudaMemcpyDeviceToHost);
 
     // Display results via console.
+    #if 0
     printf("Vector A :: \n");
     for (int i = 0; i < VECTOR_DIMENSION; i++)
     {
@@ -66,11 +70,25 @@ int main()
     {
         printf("%d ", host_vector_b[i]);
     }
+    #endif
 
-    printf("\n Vector Sum :: \n");
+    bool success = true;
     for (int i = 0; i < VECTOR_DIMENSION; i++)
     {
-        printf("%d ", host_vector_sum_result[i]);
+        if (host_vector_sum_result[i] != host_vector_a[i] + host_vector_b[i])
+        {
+            success = false;
+            break;
+        }
+        else
+        {
+            printf("%d %d = %d\n", host_vector_a[i], host_vector_b[i], host_vector_sum_result[i]);
+        }
+    }
+
+    if (success)
+    {
+        printf("Program was successfull!");
     }
 
     // Free allocated memory.
